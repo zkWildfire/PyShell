8b0b93a4d360d87586862252d4faa9a4742c6b9f
Zach Wilson <zach.wilson@wizard.dev>
2023-02-25 18:57:19 +0000

    Add additional backend tests

--- tests/unit/backends/test_dry_run_backend.py
+++ tests/unit/backends/test_dry_run_backend.py
@@ -1,15 +1,16 @@
-import os
 from pathlib import Path
 from pyshell.backends.dry_run_backend import DryRunBackend
 
 def test_run_echo():
     cmd = ["echo", "foo"]
+    cwd = "/foo/bar"
 
     backend = DryRunBackend()
-    result = backend.run(cmd, Path(os.getcwd()))
+    result = backend.run(cmd, Path(cwd))
     assert result.command == cmd[0]
     assert result.args == cmd[1:]
     assert result.full_command == " ".join(cmd)
+    assert result.cwd == str(cwd)
     assert result.output == ""
     assert result.exit_code == 0
     assert result.success
@@ -17,12 +18,14 @@ def test_run_echo():
 
 def test_run_invalid_cmd():
     cmd = ["invalid_cmd"]
+    cwd = "/foo/bar"
 
     backend = DryRunBackend()
-    result = backend.run(cmd, Path(os.getcwd()))
+    result = backend.run(cmd, Path(cwd))
     assert result.command == cmd[0]
     assert result.args == cmd[1:]
     assert result.full_command == " ".join(cmd)
+    assert result.cwd == str(cwd)
     assert result.output == ""
 
     # The dry run backend should assume that all commands succeed
--- tests/unit/backends/test_native_backend.py
+++ tests/unit/backends/test_native_backend.py
@@ -4,12 +4,23 @@ from pyshell.backends.native_backend import NativeBackend
 
 def test_run_echo():
     cmd = ["echo", "foo"]
+    cwd = os.getcwd()
 
     backend = NativeBackend()
     result = backend.run(cmd, Path(os.getcwd()))
     assert result.command == cmd[0]
     assert result.args == cmd[1:]
     assert result.full_command == " ".join(cmd)
+    assert result.cwd == str(cwd)
     assert result.output == "foo\n"
     assert result.exit_code == 0
     assert result.success
+
+
+def test_run_in_different_cwd():
+    cmd = ["echo", "foo"]
+    cwd = Path(os.getcwd()).parent
+
+    backend = NativeBackend()
+    result = backend.run(cmd, Path(cwd))
+    assert result.cwd == str(cwd)
