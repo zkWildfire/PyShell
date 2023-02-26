dd4ac6929bfbb3e34d6f7f591cf7aa701abd937b
Zach Wilson <zach.wilson@wizard.dev>
2023-02-26 18:43:58 +0000

    Add test for missed lines

---
+++ tests/integration/shell/test_run_command.py
@@ -0,0 +1,21 @@
+from pyshell import PyShell, PyShellOptions, AbortOnFailure, AllowAll, \
+    NativeBackend, NullFileLogger
+from pyshell.modules.shell import Shell
+
+def test_run_command():
+    # Initialize a PyShell instance for running commands
+    pyshell = PyShell(
+        NativeBackend(),
+        NullFileLogger(),
+        AllowAll(),
+        AbortOnFailure(),
+        PyShellOptions()
+    )
+
+    # Run the commands
+    msg = "foo bar"
+    result = Shell.run("echo", msg, pyshell)
+    assert result.success
+
+    # Verify that the output is correct
+    assert result.output == f"{msg}\n"
