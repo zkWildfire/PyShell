b978c8c61414505329f7a274d442ecb8a461265a
Zach Wilson <zach.wilson@wizard.dev>
2023-02-27 02:35:34 +0000

    Fix class missed by flags refactor

--- source/pyshell/shell/rm_command.py
+++ source/pyshell/shell/rm_command.py
@@ -9,13 +9,17 @@ class RmCommand(ExternalCommand):
     @ingroup commands
     @ingroup shell
     """
-    def __init__(self, path: str | Path, force: bool = False):
+    def __init__(self,
+        path: str | Path,
+        force: bool = False,
+        cmd_flags: CommandFlags = CommandFlags.STANDARD):
         """
         Initializes the command.
         @param path Path to remove. Can be a relative or absolute path. If the
           path is relative, it will be resolved relative to the script's current
           working directory.
         @param force If True, the command will be run with the `-f` flag.
+        @param cmd_flags The flags to set for the command.
         """
         # Determine what flags should be passed to the command
         flags: List[str] = []
@@ -26,4 +30,4 @@ class RmCommand(ExternalCommand):
             flags.append("-r")
 
         # TODO: Make this cross platform
-        super().__init__("rm", flags + [path])
+        super().__init__("rm", flags + [path], flags=cmd_flags)
